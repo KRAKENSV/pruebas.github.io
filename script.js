5b1151bf547c009c8bb7b20e45f49954ae50264e
KRAKENSV <116921533+KRAKENSV@users.noreply.github.com>
2022-10-28 15:32:03 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+
+//Suma de numeros
+
+//var numero1, numero2, total;
+
+//numero1 = prompt("Pone un número")
+
+//numero1 = parseInt(numero1);
+
+//numero2 = prompt("Pone otro número")
+
+//numero2 = parseInt(numero2);
+
+//total = numero1 + numero2;
+
+//document.write("Total: " + total);
+
+//Pide contraseña
+
+var contraseña1, contraseña2;
+
+contraseña1 = prompt("ingresa contraseña");
+
+contraseña1 = (contraseña1)
+
+contraseña2 = 12345
+
+if (contraseña1 == contraseña2) {
+    document.write("que pro, ingresaste con la contraseña correcta")
+}
+else if (contraseña1 = contraseña1) {
+    alert("¡contraseña incorrecta!")
+}
+
